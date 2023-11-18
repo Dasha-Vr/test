15c86640597095bbf2cfefe5326c81c8375b3146
Dasha-Vr <dasha-sheludyakova@yandex.ru>
2023-11-18 17:21:00 +0300

    feat: add buttons

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+const button1 = document.querySelector(".btn-warning");
+const button2 = document.querySelector(".btn.btn-success");
+const sectionOne = document.querySelector(".sectionOne");
+const itemTwo = document.querySelector(".itemTwo");
 
 
+button1.addEventListener("click", function(){
+    sectionOne.classList.toggle("sectionHide");
+})
+
+button2.addEventListener("click", function(){
+    itemTwo.classList.toggle("itemMove");
+})
 
 
-if (window.Bootstrap) {
-    console.log("Hello Dasha!")
-  } else {
-    console.log("Hello!")
-  }
 
 
 
