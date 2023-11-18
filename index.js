9fe77f1362932a07b4889dcbd232faf059c1c0c1
Dasha-Vr <dasha-sheludyakova@yandex.ru>
2023-11-18 14:44:58 +0300

    feat: add changes

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+
+
+if (window.Bootstrap) {
+    console.log("Hello Dasha!")
+  } else {
+    console.log("Hello!")
+  }
+
+
+
+
+if (window.jQuery) {
+    console.log("Hello world!")
+  } else {
+    console.log("Hello!")
+  }
+
+
